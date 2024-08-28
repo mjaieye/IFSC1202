6a80898be5a234f3dc75db9d50291ce000d3f3d8
mjaieye <mjaieye@ualr.edu>
2024-08-28 15:16:55 +0000

    po

--- 02.project.py
+++ 02.project.py
@@ -2,3 +2,6 @@
 a = float(input( "enter lenght of side a"))
 b = float(input( "enter lenght of side b"))
 c = float(input( "enter lenght of side c"))
+#calculate the semi-premiter
+s = (a+ b+ + c) / 2
+#calculate using Heron formular
