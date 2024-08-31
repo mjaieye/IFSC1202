1cc18dfb94592ab216736cd6d0c2254dd05eca9b
mjaieye <mjaieye@ualr.edu>
2024-08-31 04:18:15 +0000

    done

--- 02.project.py
+++ 02.project.py
@@ -1,3 +1,4 @@
+
 #promp for the lenght of the angle
 a = float(input( "enter lenght of side a"))
 b = float(input( "enter lenght of side b"))
@@ -5,4 +6,6 @@ c = float(input( "enter lenght of side c"))
 #calculate the semi-premiter
 s = (a+ b+ + c) / 2
 #calculate using Heron formular
-area = 
+area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+# output the result
+print(f"The area of the triangle is {area:.2f}")
