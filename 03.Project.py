32270d3a83d80c610dee01f6c4a2a0de4e1b398a
mjaieye <mjaieye@ualr.edu>
2024-09-04 15:41:56 +0000

    need to be finish

---
+++ 03.Project.py
@@ -0,0 +1,15 @@
+#enter the operator
+operator = ("enter the operator (+ - * /): ")
+#enter number
+num1 = ("enter the 1th number")
+num2 = ("enter the 2ed number")
+#enter the operation
+if operator == "+":
+    result = num1 + num2
+elif operator =="-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2
+
