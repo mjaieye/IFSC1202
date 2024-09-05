5fb849ea2b5be972a1415bde259f1acda617cdf8
mjaieye <mjaieye@ualr.edu>
2024-09-05 14:33:01 +0000

    22

--- 03.Project.py
+++ 03.Project.py
@@ -1,15 +1,21 @@
 #enter the operator
-operator = ("enter the operator (+ - * /): ")
+operator = input("enter the operator (+ - * /): ")
 #enter number
-num1 = ("enter the 1th number")
-num2 = ("enter the 2ed number")
+num1 = float(input("enter the 1th number"))
+num2 = float(input("enter the 2ed number"))
 #enter the operation
 if operator == "+":
     result = num1 + num2
+    print(result)
 elif operator =="-":
+    print(result)
     result = num1 - num2
+    print(result)
 elif operator == "*":
     result = num1 * num2
+    print(result)
 elif operator == "/":
     result = num1 / num2
-
+    print(result)
+else:
+    print(f"{operator} is not a valid operator")
