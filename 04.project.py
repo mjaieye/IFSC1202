6cefc910c920e22ed99ec30b94e531c603f85a6e
mjaieye <mjaieye@ualr.edu>
2024-09-09 15:39:32 +0000

    00

--- 04.project.py
+++ 04.project.py
@@ -1,2 +1,11 @@
-for i in range (50,100):
-    print (i)
+start = int (input("enter the starting range"))
+end = int (input("enter the ending range"))
+print ("prime number in range", start, "to", end)
+for i in range(start, end+1):
+    flag = 0
+    for n in range(2, i):
+        if (i % n == 0):
+            flag = 1
+            break
+    if (flag == 0):
+        print (i, end = ' ')
