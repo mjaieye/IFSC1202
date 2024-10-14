eac5ec243836cc510608a55830fe7731a0e9b00b
mjaieye <mjaieye@ualr.edu>
2024-10-14 14:26:05 +0000

    pp

---
+++ 08.project.py
@@ -0,0 +1,26 @@
+def main():
+    #open file and read to the list
+    with open('constitution2.txt', 'r') as file:
+        lines = [line.strip() for line in file.readlines()]
+
+    while True:
+        #promp for search
+        search_term = input("enter a search term").strip()
+        if not search_term:
+            break
+
+        #loop each line for search term
+        for i, line in enumerate(line):
+            if search_term.lower() in line.lower():
+                section_start = i
+                while section_start > 0 and lines[section_start - 1] != '':
+                    section_start -= 1
+
+                #find the end of section
+                section_end = i
+                while section_end < len(lines) - 1 and lines[section_end + 1] != '':
+                    section_end += 1
+
+                #print the search term
+        
+
