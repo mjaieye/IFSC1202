f084b02b52e725f5528c4fb3fade2e47aeb44eac
mjaieye <mjaieye@ualr.edu>
2024-10-20 03:31:11 +0000

    hh

--- 09.project.py
+++ 09.project.py
@@ -17,3 +17,24 @@ to_city = input("enter to city: ").strip()
 #search the city in the table
 from_city_index = -1
 to_city_index = -1
+
+#find the index from city
+for i, row in enumerate(data):
+    if row[0]. lower() == from_city.lower():
+        from_city_index = i
+        break
+
+#frind the index to city
+if data:
+    for j, city in enumerate(data[0]):
+        to_city_index = j
+        break
+
+#check for invalid city
+if from_city_index == -1:
+    print("invalid from city")
+elif to_city_index == -1:
+    print("invalid to city")
+else:
+    diatance = data[from_city_index][to_city_index]
+    print(f"{from_city} ot {to_city} - {diatance} miles")
